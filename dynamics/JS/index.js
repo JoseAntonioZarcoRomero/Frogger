bd8b677d33499f7869e1f856281887f4d6dc6504
JoseAntonioZarcoRomero <zarco_romero_jose_antonio@comunidad.unam.mx>
2022-05-26 21:06:06 -0500

    Fondo guía

--- dynamics/JS/index.js
+++ dynamics/JS/index.js
@@ -3,6 +3,36 @@
 var pausa = false, jugando = false;
 var tiempo=0, vidas=3, record = 9999, cronometro;
 
+class Objetito{
+    constructor(x,y,dx,dy,ruta,tx,ty){
+        const imagen = new Image();
+        imagen.src = ruta;
+        this.x=x;
+        this.y=y;
+        this.tx=tx;
+        this.ty=ty;
+        this.dx=dx;
+        this.dy=dy;
+        this.sx=0;
+        this.sy=0;
+        this.img = imagen;
+    }
+    dibuja(){
+        ctx.drawImage(this.img,this.sx,this.sy,this.tx,this.ty,this.x,this.y,this.tx,this.ty);
+    }
+    // mover(){
+    // }
+    // walkDown(){
+    // }
+    // walkUp(){
+    // }
+    // walkLeft(){
+    // }
+    // walkRight(){
+    // }
+}
+const fondo = new Objetito(0,0,0,0,"./statics/img/fondo.png",800,800);
+
 /* Canvas
 -----------------------------------------------------------------------*/
 const canvas = document.getElementById("mi-canvas");
@@ -20,21 +50,22 @@ function datosJuego(){
     ctx.textAlign = "center";
     let dato2 = "Vidas: "+vidas;
     ctx.fillText(dato2,cx,50);
-    let dato3 = "Récord: "+record+"s";
-    ctx.fillText(dato3,670,50);
     ctx.textAlign = "right";
+    let dato3 = "Récord: "+record+"s";
+    ctx.fillText(dato3,770,50);
 }
 function dibujar(){
     if(jugando === false){
         //Interfaz principal
+        fondo.dibuja();
         ctx.fillStyle = 'black';
         ctx.textAlign = "center";
         let txt1 = "OctoCross";
         ctx.font = "90px fantasy";
-        ctx.fillText(txt1,cx,cy-140);
+        ctx.fillText(txt1,cx,cy-130);
         let txt2 = "Presiona Enter para jugar";
         ctx.font = "50px monospace";
-        ctx.fillText(txt2,cx,cy+20);
+        ctx.fillText(txt2,cx,cy+10);
         let txt3 = "Presiona Escape para pausar el juego";
         ctx.font = "30px monospace";
         ctx.fillText(txt3,cx,cy+150);
@@ -46,8 +77,7 @@ function dibujar(){
             ctx.fillRect(0,0,canvas.width,canvas.height);
         } else if(pausa === false){
             //Interfaz juego
-            ctx.fillStyle = 'white';
-            ctx.fillRect(0,0,canvas.width,canvas.height);
+            fondo.dibuja();
             datosJuego();
         }
     }
