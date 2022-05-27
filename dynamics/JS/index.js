6e005a9c4ebda157e038a3d19d0adf86cbe072f0
JoseAntonioZarcoRomero <zarco_romero_jose_antonio@comunidad.unam.mx>
2022-05-26 19:05:32 -0500

    Organizando carpetas

--- dynamics/JS/index.js
+++ dynamics/JS/index.js
@@ -1,2 +1,32 @@
+/* Variables
+-----------------------------------------------------------------------*/
+var pausa = false, jugando = false;
+
+/* Canvas
+-----------------------------------------------------------------------*/
 const canvas = document.getElementById("mi-canvas");
 const ctx = canvas.getContext("2d");
+
+/* Funciones
+-----------------------------------------------------------------------*/
+function dibujar (){
+    //Interfaz
+    if(jugando === false){
+        ;
+    }
+}
+
+/* Eventos
+-----------------------------------------------------------------------*/
+window.requestAnimationFrame(dibujar);
+window.addEventListener("keyup",(evento)=>{
+    let tecla = evento.key;
+    //Comienza el juego
+    if(tecla === "Enter" && jugando === false){
+        jugando = true;
+    }
+    //Pausa el juego
+    if(tecla === " " && jugando === true){
+        pausa = (pausa === false) ? true : false;
+    }
+});
