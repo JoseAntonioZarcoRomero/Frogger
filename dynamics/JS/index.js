e51f5a67056fca65c44a2b6a6eb1ccdae953ee23
JoseAntonioZarcoRomero <zarco_romero_jose_antonio@comunidad.unam.mx>
2022-05-29 22:50:03 -0500

    fix: pausa

--- dynamics/JS/index.js
+++ dynamics/JS/index.js
@@ -22,82 +22,113 @@ class Objetito{
     }
     ArrowUp(){
         // console.log(this.y);
-        if(colision()==false){
+        // let c1=colision(malo.x,malo.y,malo.tx,malo.ty);
+        // let c2=colision(malo2.x,malo2.y,malo2.tx,malo2.ty);
+        // let c3=colision(malo3.x,malo3.y,malo3.tx,malo3.ty);
+        // if(c1==false && c2==false && c3==false){
             if(this.y>0){
                 this.y -= 50;
             }
-        } else {
-            if(vidas>0){
-                vidas--;
-                this.x=350;
-                this.y=750;
-                dibujar();
-            } else if(vidas==0){
-                perdiste();
-            }
-        }
+        // } else {
+        //     if(vidas>0){
+        //         vidas--;
+        //         this.x=350;
+        //         this.y=750;
+        //         dibujar();
+        //     } else if(vidas==0){
+        //         perdiste();
+        //     }
+        // }
     }
     ArrowDown(){
-        if(colision()==false){
+        // let c1=colision(malo.x,malo.y,malo.tx,malo.ty);
+        // let c2=colision(malo2.x,malo2.y,malo2.tx,malo2.ty);
+        // let c3=colision(malo3.x,malo3.y,malo3.tx,malo3.ty);
+        // if(c1==false && c2==false && c3==false){
             if(this.y<750){
                 this.y += 50;
             }
-        }else {
-            if(vidas>0){
-                vidas--;
-                this.x=350;
-                this.y=750;
-                dibujar();
-            } else if(vidas==0){
-                perdiste();
-            }
-        }
+        // }else {
+        //     if(vidas>0){
+        //         vidas--;
+        //         this.x=350;
+        //         this.y=750;
+        //         dibujar();
+        //     } else if(vidas==0){
+        //         perdiste();
+        //     }
+        // }
         // console.log(this.y);
     }
     ArrowRight(){
-        if(colision()==false){
+        // let c1=colision(malo.x,malo.y,malo.tx,malo.ty);
+        // let c2=colision(malo2.x,malo2.y,malo2.tx,malo2.ty);
+        // let c3=colision(malo3.x,malo3.y,malo3.tx,malo3.ty);
+        // if(c1==false && c2==false && c3==false){
             if(this.x<750){
                 this.x += 50;
             }
-        } else {
-            if(vidas>0){
-                vidas--;
-                this.x=350;
-                this.y=750;
-                dibujar();
-            } else {
-                perdiste();
-            }
-        }
+        // } else {
+        //     if(vidas>0){
+        //         vidas--;
+        //         this.x=350;
+        //         this.y=750;
+        //         dibujar();
+        //     } else {
+        //         perdiste();
+        //     }
+        // }
         // console.log(this.x);
         
     }
     ArrowLeft(){
-        if(colision()==false){
+        // let c1=colision(malo.x,malo.y,malo.tx,malo.ty);
+        // let c2=colision(malo2.x,malo2.y,malo2.tx,malo2.ty);
+        // let c3=colision(malo3.x,malo3.y,malo3.tx,malo3.ty);
+        // if(c1==false && c2==false && c3==false){
             if(this.x>0){
                 this.x -= 50;
             }
-        }else {
-            if(vidas>0){
-                vidas--;
-                this.x=350;
-                this.y=750;
-                dibujar();
-            } else if(vidas==0){
-                perdiste();
-            }
-        }
+        // }else {
+        //     if(vidas>0){
+        //         vidas--;
+        //         this.x=350;
+        //         this.y=750;
+        //         dibujar();
+        //     } else if(vidas==0){
+        //         perdiste();
+        //     }
+        // }
         // console.log(this.x);
     }
     move(){
-        this.x+=this.dx;
+        let c1=colision(this.x,this.y,this.tx,this.ty);
+        // let c2=colision(malo2.x,malo2.y,malo2.tx,malo2.ty);
+        // let c3=colision(malo3.x,malo3.y,malo3.tx,malo3.ty);
+        if(c1==false){// && c2==false && c3==false
+            this.x += this.dx;
+        } else {
+            frog.x=350;
+            frog.y=750;
+            if(vidas>1){
+                vidas--;
+                console.log(vidas);
+                dibujar();
+            } else {//if(vidas==0)
+                clearInterval(cronometro);
+                jugando=false;
+                perder=true;
+                perdiste();
+            }
+        }
     }
 }
 
 const fondo = new Objetito(0,0,0,0,"./statics/img/fondoFrogger.jpg",800,800);
-const frog = new Objetito(350,750,0,0,"./statics/img/frog.jpg",50,50);
+const frog = new Objetito(350,750,0,0,"./statics/img/a.png",50,50);//frog.jpg
 const malo = new Objetito(0,650,50,0,"./statics/img/malo.jpg",150,50);
 const malo2 = new Objetito(800,400,-50,0,"./statics/img/malo.jpg",100,50);
+const malo3 = new Objetito(0,300,100,0,"./statics/img/malo.jpg",50,50);
 
 /* Canvas
 -----------------------------------------------------------------------*/
@@ -108,38 +139,47 @@ var cx = canvas.width/2, cy = canvas.height/2;
 /* Funciones
 -----------------------------------------------------------------------*/
 function perdiste(){
-    clearInterval(cronometro);
     ctx.fillStyle = "white";
     ctx.fillRect(0,0,canvas.width,canvas.height);
     ctx.textAlign = "center";
     ctx.fillStyle = 'blue';
     ctx.fillText("Perdiste :(",cx,cy);
-    jugando=false;
-    perder=true;
 }
-function colision(){
+function colision(x1,y1,w1,h1){
     let colision;
     let x2=frog.x;
     let y2=frog.y;
     let w2=frog.tx;
     let h2=frog.ty;
-    let x1=malo.x;
-    let y1=malo.y;
-    let w1=malo.tx;
-    let h1=malo.ty;
     if(( x1 > (x2+w2) )||( (x1+w1) < x2 )||( y1 > (y2+h2) )||( (y1+h1) < y2 )){
         colision=false;
     } else {
         colision=true;
     }
-    console.log(colision);
     return colision;
 }
 function dibujaCarro(){
-    malo.dibuja();
-    malo.move();
-    malo2.dibuja();
-    malo2.move();
+    if(perder===false){
+        malo.dibuja();
+        malo.move();
+        if(malo.x+malo.tx>800){
+            malo.x=(-150);
+        }
+    }
+    if(perder===false){
+        malo2.dibuja();
+        malo2.move();
+        if(malo2.x+malo2.tx<0){
+            malo2.x=800;
+        }
+    }
+    if(perder===false){
+        malo3.dibuja();
+        malo3.move();
+        if(malo3.x+malo3.tx>800){
+            malo3.x=0;
+        }
+    }
 }
 function datosJuego(){
     ctx.fillStyle = 'white';
@@ -155,43 +195,44 @@ function datosJuego(){
     ctx.fillText(dato3,770,35);
 }
 function dibujar(){
-    if(jugando === false){
-        //Interfaz principal
-        fondo.dibuja();
-        ctx.fillStyle = 'black';
-        ctx.textAlign = "center";
-        let txt1 = "OctoCross";
-        ctx.font = "90px fantasy";
-        ctx.fillText(txt1,cx,cy-130);
-        let txt2 = "Presiona Enter para jugar";
-        ctx.font = "50px monospace";
-        ctx.fillText(txt2,cx,cy+10);
-        let txt3 = "Presiona Escape para pausar el juego";
-        ctx.font = "30px monospace";
-        ctx.fillText(txt3,cx,cy+150);
-        datosJuego();
-    } else if(jugando === true && perder == false){
-        if(pausa === true){ 
-            //Interfaz en pausa
-            ctx.fillStyle = "gray";
-            ctx.fillRect(0,0,canvas.width,canvas.height);
-        } else if(pausa === false){
-            //Interfaz juego
+    if(perder===false){
+        if(jugando === false){
+            //Interfaz principal
             fondo.dibuja();
+            ctx.fillStyle = 'black';
+            ctx.textAlign = "center";
+            let txt1 = "OctoCross";
+            ctx.font = "90px fantasy";
+            ctx.fillText(txt1,cx,cy-130);
+            let txt2 = "Presiona Enter para jugar";
+            ctx.font = "50px monospace";
+            ctx.fillText(txt2,cx,cy+10);
+            let txt3 = "Presiona Escape para pausar el juego";
+            ctx.font = "30px monospace";
+            ctx.fillText(txt3,cx,cy+150);
             datosJuego();
-            frog.dibuja();
-            dibujaCarro();
+        } else if(jugando === true){
+            if(pausa === true){ 
+                //Interfaz en pausa
+                ctx.fillStyle = "gray";
+                ctx.fillRect(0,0,canvas.width,canvas.height);
+            } else if(pausa === false){
+                //Interfaz juego
+                fondo.dibuja();
+                datosJuego();
+                frog.dibuja();
+                dibujaCarro();
+            }
         }
     }
 }
 
 /* Eventos
 -----------------------------------------------------------------------*/
-window.addEventListener("keyup",(evento)=>{
+window.addEventListener("keydown",(evento)=>{
     let tecla = evento.key;
     //Comienza el juego
     if(tecla === "Enter" && jugando === false && perder === true){
-        jugando = false;
         tiempo = 0;
         vidas = 3;
         frog.x=350;
@@ -209,6 +250,7 @@ window.addEventListener("keyup",(evento)=>{
         ctx.font = "30px monospace";
         ctx.fillText(txt3,cx,cy+150);
         datosJuego();
+        perder=false;
     }
     if(tecla === "Enter" && jugando === false && perder == false){
         jugando = true;
@@ -216,22 +258,27 @@ window.addEventListener("keyup",(evento)=>{
         vidas = 3;
         frog.x=350;
         frog.y=750;
+        malo.x = 0;
+        malo2.x = 800;
+        malo3.x = 0;
         cronometro = setInterval(()=>{
             tiempo++;
             // console.log(tiempo);
-            dibujar();
+            if(perder==false)
+                dibujar();
         },1000);
         // console.log(jugando);
         dibujar();
     }
     //Pausa el juego
-    if(tecla === " " && jugando === true){
+    if(tecla === " " && jugando === true && perder == false){
         pausa = (pausa === false) ? true : false;
         if(pausa === false){
             cronometro = setInterval(()=>{
                 tiempo++;
                 console.log(tiempo);
-                dibujar();
+                if(perder==false)
+                    dibujar();
             },1000);
         } else {
             clearInterval(cronometro);
@@ -239,16 +286,16 @@ window.addEventListener("keyup",(evento)=>{
         console.log("Pausa: "+pausa);
         dibujar();
     }
-    if(tecla === "ArrowUp" && jugando === true){
+    if(tecla === "ArrowUp" && jugando === true && perder == false){
         frog.ArrowUp();
     }
-    if(tecla === "ArrowDown" && jugando === true){
+    if(tecla === "ArrowDown" && jugando === true && perder == false){
         frog.ArrowDown();
     }
-    if(tecla === "ArrowRight" && jugando === true){
+    if(tecla === "ArrowRight" && jugando === true && perder == false){
         frog.ArrowRight();
     }
-    if(tecla === "ArrowLeft" && jugando === true){
+    if(tecla === "ArrowLeft" && jugando === true && perder == false){
         frog.ArrowLeft();
     }
     // console.log(tecla);
